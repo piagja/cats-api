aec6cd678070bc57f34d05a0a60a4d459493f1fe
Gustavo Piá <piagja@gmail.com>
2020-11-15 20:00:09 -0300

    feat: fetching api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const URL = 'https://api.thecatapi.com/v1/images/search'
+
 function App() {
+  const [cats, setCats] = useState([])
+
+  const getData = async () => {
+    const response = await fetch(URL)
+    const cats = await response.json()
+    setCats(cats)
+    console.log(cats)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-              
+      <h1>imagem</h1>        
+      <h1>infos</h1>
     </div>
   );
 }
